2fda3cf73e1700bb8254e4a4a011c12664f01f4e
Danielifed <d.danielifed@gmai.com>
2023-05-04 14:18:47 +0000

    fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,64 +1,66 @@
 #!/usr/bin/python3
-
-""" packs and deploys a new version to the server """
-
-import os
-from fabric.api import env, local, run, put
+"""
+    Fabric script that creates and distributes an archive to the web servers.
+"""
+from os import makedirs, path
 from datetime import datetime
+from fabric.api import env, local, put, run
 
-env.user = 'ubuntu'
-env.hosts = ['18.234.106.179', '34.207.83.122']
+env.hosts = ['100.26.219.124', '52.91.146.220']
 
 
-@runs_once
 def do_pack():
-    """ A function to pack the current webstatic version """
+    """ Function that generates the archive. """
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = "versions/web_static_{}.tgz".format(timestamp)
 
-    local('mkdir -p versions')
+    try:
+        makedirs("./versions", exist_ok=True)
+        local('tar -cvzf {} web_static'.format(file_path))
+        return file_path
 
-    current = datetime.now()
-    filename = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-            current.year, current.month, current.day,
-            current.hour, current.minute, current.second)
-
-    path = local('tar -cvzf {} web_static'.format(filename))
-    if path.succeeded:
-        print("Packed webstatic to {}".format(filename))
-        return filename
-    else:
+    except:
         return None
 
 
 def do_deploy(archive_path):
-    """ Deploys a webstatic to a webserver """
+    """ Function that distributes the archive.
+    Args:
+        archive_path (str): the path of the archive to deploy on the servers.
+    """
 
-    if not os.path.isfile(archive_path):
+    try:
+        if not path.exists(archive_path):
+            raise FileNotFoundError
+
+        name = archive_path.split("/")[-1]
+        name_no_ext = name.split(".")[0]
+
+        remote = "/data/web_static/releases"
+        dest = "{}/{}".format(remote, name_no_ext)
+
+        put(archive_path, '/tmp')
+        run('mkdir -p {}/'.format(dest))
+        run('tar -xzf /tmp/{} -C {}'.format(name, dest))
+        run('rm /tmp/{}'.format(name))
+        run('mv {}/web_static/* {}/'.format(dest, dest))
+        run('rm -rf {}/web_static'.format(dest))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}/ /data/web_static/current'.format(dest))
+
+    except:
+        print("Error. Version deploy aborted")
         return False
 
-    packname = archive_path.split("versions/")[-1]
-    temppath = "/tmp/{}".format(packname)
-    comppath = "/data/web_static/releases/{}/".format(packname[:-4])
-
-    put(archive_path, temppath)
-    run('mkdir -p {}'.format(comppath))
-    comp = run('tar -xzf {} -C {}'.format(temppath, comppath))
-    if comp.failed:
-        return False
-    run('rm {}'.format(temppath))
-    run('mv {}/web_static/* {}'.format(comppath, comppath))
-    run('rm -rf {}/web_static/'.format(comppath))
-    run('rm -rf /data/web_static/current')
-    run('ln -sf {} /data/web_static/current'.format(comppath))
-    print("Uploaded {} to servers".format(archive_path))
-
+    print("New version deployed!")
     return True
 
 
 def deploy():
-    """ Pack and deploy """
-
-    version = do_pack()
-    if not version:
+    """ Function that generates and distributes the archive. """
+    if archive_to_deploy:
+        return do_deploy(archive_to_deploy)
+    else:
         return False
 
-    return do_deploy(version)
+archive_to_deploy = do_pack()
